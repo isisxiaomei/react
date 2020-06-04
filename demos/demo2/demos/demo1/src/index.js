3cc02d5c02173b64a66b98e268cf32fff71562c7
isisxiaomei <xumeihong@2008.sina.com>
2020-06-04 22:32:13 +0800

    删除多余的demos文件夹

--- demos/demo2/demos/demo1/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-import Xiaojiejie from './Xiaojiejie'
-
-
-ReactDom.render(<Xiaojiejie />, document.getElementById('root'));
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import IRouter from './router';
-
-
-
-ReactDOM.render(<IRouter />, document.getElementById('root'));
